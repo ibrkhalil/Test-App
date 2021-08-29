9e6890c6bf72b5d25f56ea12fe2d758d04c0479d
Mega <vampirekid017@gmail.com>
2021-08-29 15:49:34 +0200

    Finished Infinite Scrolling Feature

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Repo from './Repo'
 import styles from './repoList.module.css'
+import InfiniteScroll from 'react-infinite-scroll-component'
 class RepoList extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            itemsArray: []
+            itemsArray: [],
+            pageNo: 2
         }
     }
 
@@ -16,12 +18,32 @@ class RepoList extends Component {
             .then(res => this.setState({ itemsArray: res.data.items }))
     }
 
+    //Get Newer items on scroll
+    fetchNewData = async () => {
+        const items = [...this.state.itemsArray]
+        await axios.get(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${this.state.pageNo}`)
+            .then(res => this.setState({ itemsArray: [...items, ...res.data.items], pageNo: this.state.pageNo + 1 }))
+
+    }
+
     render() {
         const { itemsArray } = this.state
         return (
             <div className={styles.list}>
                 {/* Map repos to a React List */}
-                {itemsArray.map(repo => <Repo key={repo.id} repo={repo} />)}
+                <InfiniteScroll
+                    dataLength={itemsArray.length}
+                    next={this.fetchNewData}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                    endMessage={
+                        <p style={{ textAlign: 'center' }}>
+                            <b>No More Items to Show</b>
+                        </p>
+                    }
+                >
+                    {itemsArray.map(repo => <Repo key={repo.id} repo={repo} />)}
+                </InfiniteScroll>
             </div>
         )
     }
